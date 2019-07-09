0d08f071d44de53ca70be5e962ac7cbddfefba50
test <test@abc.com>
2019-07-09 15:58:56 +0700

    Font ok

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 
-
 class Authen extends StatefulWidget {
   @override
   _AuthenState createState() => _AuthenState();
 }
 
 class _AuthenState extends State<Authen> {
+// Explicit
+
+// Method
+
+  Widget showLogo() {
+    return Container(
+      width: 120.0,
+      height: 120.0,
+      child: Image.asset(
+        'images/logo.png',
+      ),
+    );
+  }
+
+  Widget showText() {
+    return Text(
+      'Jacky TPA',
+      style: TextStyle(
+        fontSize: 30.0,
+        color: Colors.green[900],
+        fontWeight: FontWeight.bold,
+        fontFamily: 'Mitr',
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Authen'),);
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 60.0),
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: <Widget>[showLogo(), showText()],
+        ),
+      ),
+    );
   }
 }
